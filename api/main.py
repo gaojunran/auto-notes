f2457ac2b6b494640a4ee90cad68e8b8d3be72d1
gaojunran <nebula2021@126.com>
2025-01-27 18:35:19 +0800

    chore: 重构后端接口测试数据

--- api/main.py
+++ api/main.py
@@ -3,26 +3,20 @@ import time
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 
-from models import NoteRoute, RawRecognition, Point, Link, Subtitle, Lecture, Node, NodeLink, NodeCategory
+from models import (
+    NoteRoute,
+    RawRecognition,
+    Point,
+    Link,
+    Subtitle,
+    Lecture,
+    Node,
+    NodeLink,
+    NodeCategory,
+)
 
 app = FastAPI()
 
-TEST_MARKDOWN = """
-以下为CommonMark Markdown的语法示例：
-# 一级标题
-（这里建议只包含一级标题，因为我们在笔记左侧已经有一级子主题了）
-
-**加粗**，*斜体*，~~删除线~~，`python main.py`，$E=mc^2$，
-
-有序列表
-1. 第一项
-2. 第二项
-3. 第三项
-
-无序列表
-- 第一项
-- 第二项
-"""
 
 # 定义请求和响应模型
 class RecordResponse(BaseModel):
@@ -32,16 +26,20 @@ class RecordResponse(BaseModel):
     abstract: str
     raw_recognition: list[RawRecognition]
 
+
 class NoteRequest(BaseModel):
     topic: str
     abstract: str
     raw_recognition: list[RawRecognition]
 
+
 class NoteResponse(BaseModel):
     points: list[Point]
 
+
 class NetworkRequest(BaseModel):
     lectures: list[Lecture]
+
 
 class NetworkResponse(BaseModel):
     nodes: list[Node]
@@ -53,18 +51,23 @@ class NetworkResponse(BaseModel):
 async def test():
     return "Hello, World!"
 
+
 @app.post("/record", response_model=RecordResponse)
 async def post_record(file: UploadFile):
-    time.sleep(2) # 模拟延时
+    time.sleep(2)  # 模拟延时
     return RecordResponse(
-        id=int(time.time()),
-        duration=45*60,
+        id=12345678,
+        duration=45 * 60,
         topic="布尔代数",
         abstract="布尔代数是数学的一个分支，它研究的是命题的真值表。",
         raw_recognition=[
-            RawRecognition(start=0, end=12, text="命题的真值表是...."),
-            RawRecognition(start=13, end=25, text="命题的真值表有几种形式？"),
-        ]
+            RawRecognition(start=0, end=12, text="什么是布尔值？"),
+            RawRecognition(start=13, end=25, text="布尔值有什么用？"),
+            RawRecognition(start=26, end=30, text="布尔值有哪两种？"),
+            RawRecognition(start=31, end=40, text="什么是命题真值？"),
+            RawRecognition(start=41, end=50, text="什么是真值表？"),
+            RawRecognition(start=51, end=60, text="真值表有什么用？"),
+        ],
     )
 
 
@@ -73,47 +76,109 @@ async def get_note(note: NoteRequest):
     time.sleep(2)  # 模拟延时
     return NoteResponse(
         points=[
-            Point(importance=2, name="布尔值", summary="布尔值是逻辑中的基本概念，它只有两个取值：真和假。", links=[
-                Link(name="维基百科", href="https://zh.wikipedia.org/wiki/%E5%B8%83%E5%B0%94%E5%80%BC"),
-                Link(name="布尔代数", href="https://zh.wikipedia.org/wiki/%E5%B8%83%E5%B0%94%E4%BB%A3%E6%95%B0"),
-            ], subtitles=[
-                Subtitle(subtitle="真值表",
-                         md=TEST_MARKDOWN,
-                         raw_recognition=[
-                             RawRecognition(start=0, end=12, text="真值表的形式有几种形式？"),
-                             RawRecognition(start=13, end=25, text="真值表有几种形式？"),
-                         ]
-                ),
-                Subtitle(subtitle="真值",
-                         md="**真值**是指命题在某种特定条件下取真的结果。",
-                         raw_recognition=[
-                             RawRecognition(start=22 * 60, end=22 * 60 + 12, text="什么是真值？"),
-                             RawRecognition(start=22 * 60 + 13, end=22 * 60 + 25, text="真值有什么用？"),
-                         ]
-                ),
-            ])
+            Point(
+                importance=2,
+                name="布尔值",
+                summary="布尔值是逻辑中的基本概念，它只有两个取值：真和假。",
+                links=[
+                    Link(
+                        name="维基百科",
+                        href="https://zh.wikipedia.org/wiki/%E5%B8%83%E5%B0%94%E5%80%BC",
+                    ),
+                    Link(
+                        name="百度百科",
+                        href="https://baike.baidu.com/item/%E5%B8%83%E5%B0%94%E5%80%BC",
+                    ),
+                ],
+                subtitles=[
+                    Subtitle(
+                        subtitle="布尔值的定义",
+                        md="**布尔值**是逻辑中的基本概念，它只有两个取值：真和假。",
+                        raw_recognition=[
+                            RawRecognition(start=0, end=12, text="什么是布尔值？"),
+                            RawRecognition(start=13, end=25, text="布尔值有什么用？"),
+                        ],
+                    ),
+                    Subtitle(
+                        subtitle="布尔值的类型",
+                        md="布尔值有两种类型：命题真值和命题真值表。",
+                        raw_recognition=[
+                            RawRecognition(start=26, end=30, text="布尔值有哪两种？"),
+                            RawRecognition(start=31, end=40, text="什么是命题真值？"),
+                        ],
+                    ),
+                ],
+            ),
+            Point(
+                importance=3,
+                name="真值表",
+                summary="真值表是指命题的真值对所有可能的取值组合的一种表示。",
+                links=[
+                    Link(
+                        name="维基百科",
+                        href="https://zh.wikipedia.org/wiki/%E7%9C%9F%E5%80%BC%E8%A1%A8",
+                    ),
+                    Link(
+                        name="百度百科",
+                        href="https://baike.baidu.com/item/%E7%9C%9F%E5%80%BC%E8%A1%A8",
+                    ),
+                ],
+                subtitles=[
+                    Subtitle(
+                        subtitle="真值表的定义",
+                        md="**真值表**是指命题的真值对所有可能的取值组合的一种表示。",
+                        raw_recognition=[
+                            RawRecognition(start=41, end=50, text="什么是真值表？"),
+                            RawRecognition(start=51, end=60, text="真值表有什么用？"),
+                        ],
+                    ),
+                ],
+            ),
         ]
     )
+
 
 @app.post("/network", response_model=NetworkResponse)
 async def get_network(network: NetworkRequest):
     time.sleep(2)  # 模拟延时
     return NetworkResponse(
         nodes=[
-            Node(name="布尔代数", category=0, size=5, route=NoteRoute(id=1737889928)), # topic
-            Node(name="真值表", category=0, size=1, route=NoteRoute(id=1737889928, point="真值表")), # point
-            Node(name="真值", category=0, size=4, route=NoteRoute(id=1737889928, point="真值")), # point
-            Node(name="另一个point", category=1, size=2, route=NoteRoute(id=123, point="另一个point")), # point
-            Node(name="另一个topic", category=1, size=5, route=NoteRoute(id=123)), # topic
+            Node(
+                name="布尔代数", category=0, size=5, route=NoteRoute(id=12345678)
+            ),  # topic
+            Node(
+                name="布尔值",
+                category=0,
+                size=2,
+                route=NoteRoute(id=12345678, point="布尔值"),
+            ),  # point
+            Node(
+                name="真值表",
+                category=0,
+                size=3,
+                route=NoteRoute(id=12345678, point="真值表"),
+            ),  # point
+            Node(
+                name="另一个topic",
+                category=1,
+                size=4,
+                route=NoteRoute(id=12345679),
+            ),  # point
+            Node(
+                name="另一个point",
+                category=1,
+                size=1,
+                route=NoteRoute(id=12345679, point="另一个point"),
+            ),  # point
         ],
         links=[
             NodeLink(source="布尔代数", target="真值表", weight=1),
-            NodeLink(source="布尔代数", target="真值", weight=2),
+            NodeLink(source="布尔代数", target="布尔值", weight=2),
             NodeLink(source="真值表", target="另一个point", weight=1),
             NodeLink(source="另一个topic", target="另一个point", weight=4),
         ],
         categories=[
             NodeCategory(idx=0, name="布尔代数"),
             NodeCategory(idx=1, name="另一个topic"),
-        ]
+        ],
     )
